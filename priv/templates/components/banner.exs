67f0345573b1b68d5baaaf4534fc9c695c200dec
Mona Aghili <mona.laghili@gmail.com>
2024-11-18 13:32:16 +0330

    Feat: Add gradient and bordered variants to banner (#109)
    * Feat: Add gradient and bordered variants to banner
    
    * Add colors and gradients to compoennt
    
    * Add gradient to list of `border_class` function
    
    * Resolve diffs
    
    * Remove extra enter

--- priv/templates/components/banner.exs
+++ priv/templates/components/banner.exs
@@ -2,9 +2,11 @@
   banner: [
     name: "banner",
     args: [
-      variant: ["default", "outline", "transparent", "shadow"],
+      variant: ["default", "outline", "transparent", "shadow", "bordered", "gradient"],
       color: [
         "natural",
+        "white",
+        "dark",
         "primary",
         "secondary",
         "success",
