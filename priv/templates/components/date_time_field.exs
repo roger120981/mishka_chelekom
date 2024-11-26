eedbfad1103343f64344c774d6b39812fc7d497b
Mona Aghili <mona.laghili@gmail.com>
2024-11-26 21:48:18 +0330

    Chore: Modify colors of date time and add dark mode (#146)
    * Chore: add colors variant of date time form
    
    * Fix border, and class name
    
    * Fix class names
    
    * Fix color in bordered variant
    
    * Fix issue
    
    * fix issue
    
    * Add helpers to `exs` file
    
    ---------
    
    Co-authored-by: Shahryar Tavakkoli <shahryar.tbiz@gmail.com>

--- priv/templates/components/date_time_field.exs
+++ priv/templates/components/date_time_field.exs
@@ -2,8 +2,9 @@
   date_time_field: [
     name: "date_time_field",
     args: [
-      variant: ["outline", "default", "unbordered", "shadow", "transparent"],
+      variant: ["outline", "default", "bordered", "shadow", "transparent"],
       color: [
+        "natural",
         "white",
         "primary",
         "secondary",
@@ -12,7 +13,6 @@
         "warning",
         "danger",
         "info",
-        "light",
         "misc",
         "dawn",
         "silver"
@@ -21,6 +21,7 @@
       space: ["extra_small", "small", "medium", "large", "extra_large"],
       rounded: ["extra_small", "small", "medium", "large", "extra_large"],
       only: ["date_time_field"],
+      helpers: [],
       module: ""
     ],
     optional: [],
