a8e673af94918bc55caabd1694e60c3389e19827
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-12-06 00:55:44 +0330

    Release 0.0.2-alpha.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MishkaChelekom.MixProject do
   use Mix.Project
 
-  @version "0.0.2-alpha.1"
+  @version "0.0.2-alpha.2"
   @source_url "https://github.com/mishka-group/mishka_chelekom"
 
   def project do
