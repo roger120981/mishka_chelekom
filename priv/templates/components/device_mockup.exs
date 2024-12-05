df4890607f9c25e7e5b1ff57b8d5e196184317e3
Mona Aghili <mona.laghili@gmail.com>
2024-12-05 19:37:13 +0330

    Chore: Modify colors of device mockup, add dark mode (#167)
    * Chore: Modify colors of device mockup, add dark mode
    
    * Update device_mockup.exs
    
    ---------
    
    Co-authored-by: Shahryar Tavakkoli <shahryar.tbiz@gmail.com>

--- priv/templates/components/device_mockup.exs
+++ priv/templates/components/device_mockup.exs
@@ -3,21 +3,20 @@
     name: "device_mockup",
     args: [
       color: [
-        "white",
+        "natural",
         "primary",
         "secondary",
-        "dark",
         "success",
         "warning",
         "danger",
         "info",
-        "light",
         "misc",
         "dawn",
         "silver"
       ],
       type: ["watch", "android", "laptop", "ipad", "imac"],
       only: ["device_mockup"],
+      helpers: [],
       module: ""
     ],
     optional: [],
