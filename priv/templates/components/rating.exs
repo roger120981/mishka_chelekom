58254e37cc15f54aafc442462076996f5a8e8317
Mona Aghili <mona.laghili@gmail.com>
2024-12-30 14:16:16 +0330

    Feat: add base color to rating (#234)
    * Feat: add base color to rating
    
    * fix issue

--- priv/templates/components/rating.exs
+++ priv/templates/components/rating.exs
@@ -3,6 +3,8 @@
     name: "rating",
     args: [
       color: [
+        "base",
+        "natural",
         "white",
         "primary",
         "secondary",
@@ -11,7 +13,7 @@
         "warning",
         "danger",
         "info",
-        "light",
+        "silver",
         "misc",
         "dawn"
       ],
