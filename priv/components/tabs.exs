b03bb06e8d4bf0289628ab70243616896467e308
Mona Aghili <mona.laghili@gmail.com>
2025-01-15 11:03:37 +0330

    Refactor: add new styles to tabs (#273)

--- priv/components/tabs.exs
+++ priv/components/tabs.exs
@@ -16,7 +16,6 @@
         "misc",
         "dawn"
       ],
-      rounded: ["extra_small", "small", "medium", "large", "extra_large", "full", "none"],
       size: ["extra_small", "small", "medium", "large", "extra_large"],
       padding: ["extra_small", "small", "medium", "large", "none"],
       only: ["tabs"],
