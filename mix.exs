067afa7609c02386e6f29c3a01de5489e0e85e72
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-06-10 17:40:18 +0330

    Fix Typo of version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule MishkaChelekom.MixProject do
   use Mix.Project
 
-  @version "0.0.6-alpha"
+  @version "0.0.6"
   @source_url "https://github.com/mishka-group/mishka_chelekom"
 
   def project do
